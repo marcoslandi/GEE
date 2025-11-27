483496b888acedaf7bbfa91a64b04b25680be3b8
marcoslandi <marcoslandi1980@gmail.com>
2025-11-27 15:04:33 -0300

    Update maskS2CloudsII.js
    Descripción de la función

--- Funciones/Sentinel 2 MSI/maskS2CloudsII.js
+++ Funciones/Sentinel 2 MSI/maskS2CloudsII.js
@@ -1,5 +1,12 @@
 // Función para aplicar máscara de nube
 function maskS2Clouds(image){
+  /*
+  La función selecciona los píxeles de agua, suelo desnudo y
+  vegetación mediante la banda SCL.
+  
+  Los pixeles de las categorías no válidas no se tienen en cuenta
+  se eliminan del procesamiento
+  */
   
   //Selección banda SCL
   var sclBand = image.select('SCL');
